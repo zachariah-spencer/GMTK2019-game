443b499fa93deed05056b9a46a3dd020007472c4
cslaf <cade.schlaefli@gmail.com>
2019-08-02 22:15:49 -0500

    tinkering with projectiles

--- boss/projectiles/Projectile.gd
+++ boss/projectiles/Projectile.gd
@@ -1,18 +1,23 @@
 extends Node2D
 
-var damage = 3
+var damage := 1
 var knockback = Vector2.ZERO
 var type = 0
 var direction = Vector2.LEFT
 var speed = 70
+var lifetime := 3
 
 var time = PI
 
 onready var fizzle_timer = $FizzleTimer
 
+func _ready():
+	$LifeTimer.start(lifetime)
+
 func _physics_process(delta):
 	if !fizzle_timer.is_stopped() :
 		modulate.a = fizzle_timer.time_left
+		$CollisionShape2D.shape.radius *= .8
 	else :
 		_move(delta)
 	time += delta
@@ -23,14 +28,13 @@ func _draw():
 
 
 func _move(delta : float):
-#	position += Vector2.UP.rotated(direction.angle()) * sin(time*10)
 	position += speed*direction*delta
 
 func _on_hit(body):
 	if body.has_method("hit") :
 		body.hit(self, damage, type, knockback)
-
 	fizzle()
 
 func fizzle():
-	$FizzleTimer.start()
+	if $FizzleTimer.is_stopped() :
+		$FizzleTimer.start()
