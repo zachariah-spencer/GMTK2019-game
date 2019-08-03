d4bf2f869ebfda8504cd8ac56183a893f0bc3110
zachariah-spencer <spencerzachariah@gmail.com>
2019-08-03 14:06:24 -0500

    begin work on charges

--- map/ChargeSpawner.gd
+++ map/ChargeSpawner.gd
@@ -2,6 +2,19 @@ extends Node2D
 
 export(NodePath) var charge
 onready var spawn_pos := $SpawnPos
+onready var spawned_charges := $SpawnedCharges
+onready var cd_timer := $CooldownTimer
 
 func _ready():
-	pass
+	_spawn_charge()
+
+
+func _spawn_charge():
+	var to_add = charge.instance()
+	to_add.global_position = spawn_pos.global_position
+	spawned_charges.add_child(to_add)
+
+
+func _on_CooldownTimer_timeout():
+	if $SpawnedCharges.get_children().empty():
+		_spawn_charge()
---
+++ map/charges/Charge.gd
@@ -0,0 +1,7 @@
+extends Area2D
+class_name Charge
+
+
+
+func _on_Charge_body_entered(body):
+	var player = body
