1d45758845a6d02ba15ae80964f8169e9f0e2f28
zachariah-spencer <spencerzachariah@gmail.com>
2019-08-04 11:15:18 -0500

    making lightning obstruction clearer

--- map/obstructions/LightningObstruction.gd
+++ map/obstructions/LightningObstruction.gd
@@ -1,5 +1,17 @@
 extends Obstruction
 
+var activated := false
+
 func _ready():
 	pass # Replace with function body.
 
+func hit(by : Node2D, damage : int, type : int, knockback : Vector2):
+	if type ==  activation_type :
+		activated = true
+		emit_signal("activate")
+		activate()
+
+func _physics_process(delta):
+	if activated && modulate.r < 1:
+			modulate.r += delta
+			modulate.g += delta
