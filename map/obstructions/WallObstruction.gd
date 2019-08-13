9a48b5465cfaceed304c8686ce90e7cd1baac634
cslaf <cade.schlaefli@gmail.com>
2019-08-12 20:49:24 -0500

    add crush code to Wall obstruction

--- map/obstructions/WallObstruction.gd
+++ map/obstructions/WallObstruction.gd
@@ -10,3 +10,14 @@ func activate():
 	else :
 		open = false
 		$AnimationPlayer.play_backwards("Open")
+		yield(get_tree().create_timer(.45), "timeout")
+		var crushed = $CrushCheck.get_overlapping_bodies()
+		if crushed.size() > 0 :
+			for crush in crushed :
+				if crush.has_method("hit") :
+					crush.hit(self, 1, 0, Vector2.ZERO)
+			$AnimationPlayer.stop()
+			open = true
+			$AnimationPlayer.play("Open")
+
+
